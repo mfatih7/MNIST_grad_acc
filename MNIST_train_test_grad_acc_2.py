e3a33299f0e577691ffb803b433da0900597d9f7
mfatih7 <mfatihaydogdu@gmail.com>
2023-07-30 21:47:12 +0300

    scripts updated

---
+++ MNIST_train_test_grad_acc_2.py
@@ -0,0 +1,155 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+
+from models.models import get_model
+from utils import get_gpu_utilization
+from utils import get_mem_info
+
+
+def train_and_test( data_path, learning_rate, n_epochs, num_workers, batch_sizes, optimizing_batches, optimizer_types ):
+
+    transform = transforms.Compose([
+        transforms.ToTensor(),           # Convert PIL images to tensors
+        transforms.Normalize((0.5,), (0.5,))   # Normalize the image data to the range [-1, 1]
+    ])
+    
+    train_dataset = torchvision.datasets.MNIST(    root = data_path,
+                                                   train = True,
+                                                   download = True, 
+                                                   transform = transform )
+    
+    
+    test_dataset = torchvision.datasets.MNIST(     root = data_path,
+                                                   train = False,
+                                                   download = True, 
+                                                   transform = transform )
+    tot_iter = 0
+    for batch_size_idx, batch_size in enumerate(batch_sizes):
+        tot_iter += len(optimizing_batches[batch_size_idx] * len(optimizer_types) )
+    
+    train_acc_vec = np.zeros( ( tot_iter, n_epochs ) ) 
+    test_acc_vec = np.zeros( ( tot_iter, n_epochs ) ) 
+    train_time_vec = np.zeros( ( tot_iter, n_epochs ) ) 
+    test_time_vec = np.zeros( ( tot_iter, n_epochs ) ) 
+    
+    iter_index = 0
+    for batch_size_idx, batch_size in enumerate(batch_sizes):
+        for optimizing_batch in optimizing_batches[batch_size_idx]:
+            for optimizer_type in optimizer_types:
+    
+                device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+                model = get_model().to(device)
+                criterion = nn.CrossEntropyLoss()            
+    
+                if(optimizer_type == 'Adam'):
+                    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+                else:
+                    optimizer = optim.SGD(model.parameters(), lr=learning_rate)
+                
+                # Training loop
+                print("-" * 80)
+                print("Started Training and Test " + str(iter_index) + '/' + str(tot_iter-1) + " for batch size " + str(batch_size) +
+                      ", optimizing batch " + str(optimizing_batch) + ", optimizer type " + optimizer_type )            
+                
+                for epoch in range(n_epochs):
+                    
+                    correct_train = 0
+                    total_train = 0
+                    
+                    correct_test = 0
+                    total_test = 0
+                    
+                    torch.cuda.reset_peak_memory_stats()
+                    
+                    start_time_train = time.perf_counter()
+                    train_dataloader = DataLoader(  dataset = train_dataset,
+                                                    batch_size = batch_size,
+                                                    shuffle = True,
+                                                    pin_memory = True,
+                                                    num_workers = num_workers ) 
+                    
+                    for i, (images, labels) in enumerate(train_dataloader):
+                        images, labels = images.to(device), labels.to(device)
+                
+                        # Forward pass
+                        outputs = model(images)
+                        loss = criterion(outputs, labels)
+                        
+                        loss = loss / optimizing_batch
+                        
+                        # Backward and optimize                    
+                        loss.backward()
+                        
+                        _, predicted = torch.max(outputs.data, 1)
+                        total_train += labels.size(0)
+                        correct_train += (predicted == labels).sum().cpu().detach().numpy()
+                        acc_train = correct_train / total_train
+                        
+                        if( (i+1) % optimizing_batch == 0 or (i+1) == len(train_dataloader) ):
+                            optimizer.step()
+                            optimizer.zero_grad()       
+
+                            print("Train Epoch {}/{} Batch {}/{} LR {:.6f} Loss {:.6f} CorPred {}/{} Acc {:.6f}"
+                                    .format(    epoch,
+                                                n_epochs-1,
+                                                i,
+                                                len(train_dataloader)-1,
+                                                learning_rate,
+                                                loss.cpu().detach().numpy(),
+                                                correct_train,
+                                                total_train,
+                                                acc_train) )                 
+                    
+                    train_acc_vec[iter_index, epoch] = acc_train
+                    end_time_train = time.perf_counter()
+                    train_time_vec[iter_index, epoch] = end_time_train - start_time_train
+                    
+                    torch.cuda.reset_peak_memory_stats()
+                    
+                    start_time_test = time.perf_counter()
+                    test_dataloader = DataLoader( dataset = test_dataset,
+                                                  batch_size = batch_size,
+                                                  shuffle = False,
+                                                  pin_memory = True,
+                                                  num_workers = num_workers )
+    
+                    model.eval()  # Sets the model to evaluation mode
+                    with torch.no_grad():        
+                        for i, (images, labels) in enumerate(test_dataloader):
+                            images, labels = images.to(device), labels.to(device)       
+                            
+                            outputs = model(images)        
+                            
+                            _, predicted = torch.max(outputs.data, 1)
+                            total_test += labels.size(0)
+                            correct_test += (predicted == labels).sum().cpu().detach().numpy()
+                            acc_test = correct_test / total_test
+                            
+                            if( (i+1) % optimizing_batch == 0 or (i+1) == len(test_dataloader) ):
+                                print("Test Epoch {}/{} Batch {}/{} LR {:.6f} CorPred {}/{} Acc {:.6f}"
+                                        .format(    epoch,
+                                                    n_epochs-1,
+                                                    i,
+                                                    len(test_dataloader)-1,
+                                                    learning_rate,
+                                                    correct_test,
+                                                    total_test,
+                                                    acc_test) )
+
+                    test_acc_vec[iter_index, epoch] = acc_test
+                    end_time_test = time.perf_counter()
+                    test_time_vec[iter_index, epoch] = end_time_test - start_time_test
+                            
+                    print("-" * 40)
+                iter_index = iter_index + 1
+
+    return ( train_acc_vec, test_acc_vec, train_time_vec, test_time_vec )
+    
+    
--- run_MNIST_train_test_grad_acc.py
+++ run_MNIST_train_test_grad_acc.py
@@ -1,44 +1,63 @@
-import MNIST_train_test_grad_acc
+# import MNIST_train_test_grad_acc
+import MNIST_train_test_grad_acc_2
+
 
 data_path = './'
 
 # Hyperparameters
 learning_rate = 0.001
 n_epochs = 3
-num_workers = 0
-
-# batch_sizes = [ 128, 128, 128, 128, 128, 128, 128, 128, 
-#                   64,  64,  64,  64,  64,  64,  64, 
-#                   32,  32,  32,  32,  32,  32,
-#                   16,  16,  16,  16,  16, ]
-# optimizing_batches = [    [ 128, 64, 32, 16,  8,  4,  2,  1, ],
-#                           [ 64,  32, 16,  8,  4,  2,  1, ],
-#                           [ 32,  16,  8,  4,  2,  1, ],
-#                           [ 16,   8,  4,  2,  1 ], ]
-# optimizer_types = ['SGD', 'Adam']
-
-batch_sizes = [  128,
-                 64,
-                 32,
-                 16,
-                 8,  ]
-
-optimizing_batches = [  [ 128, ], 
-                        [ 64, ], 
-                        [ 32, ], 
-                        [ 16, ], 
-                        [ 8, ], ]
-optimizer_types = ['SGD']
+num_workers = 1
 
 
+# batch_sizes = [  128, 
+#                   64,
+#                   32,
+#                   16,
+#                    8,
+#                    4,  ]
+
+# optimizing_batches = [  [  128,  ], 
+#                         [  64,  128,  ],
+#                         [  32,   64,   128,  ], 
+#                         [  16,   32,    64,   128,  ], 
+#                         [   8,   16,    32,    64,   128,  ], 
+#                         [   4,    8,    16,    32,    64,   128,  ],  ]
+
+# optimizer_types = ['SGD', 'ADAM']
+
+batch_sizes = [  128, 
+                  64,
+                  32,  ]
+
+optimizing_batches = [  [  128,  ], 
+                        [  64,  128,  ],
+                        [  32,   64,   128,  ],]
+
+optimizer_types = ['SGD', 'ADAM']
+
+
+# if __name__ == '__main__':
+#     results = MNIST_train_test_grad_acc.train_and_test(  data_path,
+#                                                          learning_rate,
+#                                                          n_epochs,
+#                                                          num_workers,
+#                                                          batch_sizes,
+#                                                          optimizing_batches,
+#                                                          optimizer_types  )    
+#     train_acc_vec = results[0]
+#     test_acc_vec = results[1]
+#     train_time_vec = results[2]
+#     test_time_vec = results[3]
+    
 if __name__ == '__main__':
-    results = MNIST_train_test_grad_acc.train_and_test(  data_path,
-                                                         learning_rate,
-                                                         n_epochs,
-                                                         num_workers,
-                                                         batch_sizes,
-                                                         optimizing_batches,
-                                                         optimizer_types  )    
+    results = MNIST_train_test_grad_acc_2.train_and_test(   data_path,
+                                                            learning_rate,
+                                                            n_epochs,
+                                                            num_workers,
+                                                            batch_sizes,
+                                                            optimizing_batches,
+                                                            optimizer_types,  )    
     train_acc_vec = results[0]
     test_acc_vec = results[1]
     train_time_vec = results[2]
