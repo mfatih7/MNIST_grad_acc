dd2e9332a958334b18de18b2fe73dc68a918494d
mfatih7 <mfatihaydogdu@gmail.com>
2023-08-10 22:18:22 +0300

    plot gpu util added

--- plots.py
+++ plots.py
@@ -54,7 +54,7 @@ def plot_GPU_mem_used( output_path, input_size, training_params, optimizer_types
             plt.plot(x_line, y_line, label=legend_name, marker='o')
             
         plt.grid(True)
-        plt.legend(title='Inpu Size', fontsize = 'small')
+        plt.legend(title='Input Size', fontsize = 'small')
         plt.ylabel('Average GPU Memory Usage')
         plt.xlabel('Batch')
         plt.title( device_name + ' ' + str(device_mem_cap) + ' MB ' + optimizer_type + ' opt' )
@@ -66,6 +66,69 @@ def plot_GPU_mem_used( output_path, input_size, training_params, optimizer_types
         if not os.path.exists( folder_path ):
             os.makedirs(folder_path)            
         plt.savefig( os.path.join(folder_path, plot_file_name), dpi=300)            
+
+def plot_GPU_util( output_path, input_size, training_params, optimizer_types, gpu_util, device_name, device_mem_cap):
+    
+    gpu_util_mean_of_epochs = gpu_util.mean(axis=1)    
+    gpu_util_mean_of_epochs = gpu_util_mean_of_epochs.reshape(-1, len(optimizer_types) )
+    
+    x_ticks = []    
+    for training_param in training_params:
+        x_ticks.append(training_param[1])
+    x_ticks = sorted( list( set(x_ticks) ) )
+    
+    for optimizer_type_id, optimizer_type in enumerate(optimizer_types):
+    
+        plt.figure()                           
+        x_lines = []
+        y_lines = []
+        legend_names = []
+        start_ind = 0
+        list_ind = 0        
+        
+        while ( list_ind < gpu_util_mean_of_epochs.shape[0] ):
+            x_line = []
+            y_line = []
+            start_ind = list_ind
+            cur_exp_rat = training_params[list_ind][0]
+            cur_batch = training_params[list_ind][1]
+            list_ind = list_ind + 1
+            while ( list_ind < gpu_util_mean_of_epochs.shape[0] ):
+                if(cur_exp_rat == training_params[list_ind][0] and cur_batch == training_params[list_ind][1]):
+                    list_ind = list_ind + 1
+                elif(cur_exp_rat == training_params[list_ind][0] and cur_batch != training_params[list_ind][1]):
+                    y_line.append( max(gpu_util_mean_of_epochs[ start_ind:list_ind, optimizer_type_id]) )
+                    x_line.append( x_ticks.index(cur_batch) )
+                    start_ind = list_ind
+                    cur_exp_rat = training_params[list_ind][0]
+                    cur_batch = training_params[list_ind][1]
+                    list_ind = list_ind + 1
+                else:
+                    break
+                
+            y_line.append( max(gpu_util_mean_of_epochs[ start_ind:list_ind, optimizer_type_id]) )
+            x_line.append( x_ticks.index(cur_batch) )
+            
+            y_lines.append(y_line)
+            x_lines.append(x_line)
+            legend_names.append( str(input_size*cur_exp_rat) + 'x' + str(input_size*cur_exp_rat) )
+            
+        for x_line, y_line, legend_name in zip(x_lines, y_lines, legend_names):
+            plt.plot(x_line, y_line, label=legend_name, marker='o')
+            
+        plt.grid(True)
+        plt.legend(title='Input Size', fontsize = 'small')
+        plt.ylabel('Average GPU Util')
+        plt.xlabel('Batch')
+        plt.title( device_name + ' ' + str(device_mem_cap) + ' MB ' + optimizer_type + ' opt' )
+        
+        plt.xticks( range(len(x_ticks)), [str(x_tick) for x_tick in x_ticks] )
+        
+        plot_file_name = 'gpu_util' + optimizer_type + '.png'
+        folder_path = os.path.join(output_path, device_name)
+        if not os.path.exists( folder_path ):
+            os.makedirs(folder_path)            
+        plt.savefig( os.path.join(folder_path, plot_file_name), dpi=300)    
 
 def plot_train_time( output_path, input_size, training_params, optimizer_types, train_time, device_name, device_mem_cap):
     
